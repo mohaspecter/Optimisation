3facb7657e3c3b8216f1c300100bde19b480486e
Moha Specter <drame.moha44@gmail.com>
2020-11-15 23:54:07 +0100

    fichier lp corrigé

--- Optimisation/modele.c
+++ Optimisation/modele.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "modele.h"
 
 void createLP(char* nomFichier){
@@ -50,13 +52,13 @@ void createLP(char* nomFichier){
 	//valeurs
 	for (i = 0; i < N; i++)
 	{
-		tab[i][0] = i;
+		tab[i][0] = i+1;
 	}
 
 	//cout
 	int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12;
 	int b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
-	int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12;
+	int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11;
 
 
 	//si la ligne est complète on rempli le tableau avec les données de la ligne
